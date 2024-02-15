eb0423298d113196268b68e1243bb29a84893fe9
Syed Mohammad Mehdi <94230205+Mehdi9721@users.noreply.github.com>
2024-02-15 21:23:01 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,13 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin
-=======
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000")
->>>>>>> 5f449b9 (Created REST API for Exam)
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
