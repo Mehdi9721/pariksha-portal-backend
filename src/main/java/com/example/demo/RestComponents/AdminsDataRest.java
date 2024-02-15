64a3888efc5384e9038ba0746919169653605c15
Syed Mohammad Mehdi <94230205+Mehdi9721@users.noreply.github.com>
2024-02-15 21:24:54 +0530

    Update AdminsDataRest.java

--- src/main/java/com/example/demo/RestComponents/AdminsDataRest.java
+++ src/main/java/com/example/demo/RestComponents/AdminsDataRest.java
@@ -36,11 +36,5 @@ public int adminLogin(@RequestBody AdminsDataModel adminModel) {
 	}
 	
 	return 1;
-=======
-	if(adminAuth!=null) {
-		return 1;
-	}
-	return 0;
->>>>>>> 5f449b9 (Created REST API for Exam)
 	}
 }
